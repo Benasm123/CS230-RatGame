d08729c84b44dfc936bde66b9c25c2b4c4f49b94
TimiAbajingin123 <54741402+TimiAbajingin123@users.noreply.github.com>
2021-11-17 15:01:58 +0000

    Update Rat.java

--- src/RatGame/Rat.java
+++ src/RatGame/Rat.java
@@ -12,44 +12,45 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.canvas.Canvas;
 import java.util.Random;
 
-public class Rats {
+public class Rat {
     private static final int WINDOW_WIDTH = 40;
     private static final int WINDOW_HEIGHT = 10;
     
     Image maleRat = new Image("Assets/Male.png");
     Image femaleRat = new Image("Assets/Female.png");
     Canvas canvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+
     Random rnd = new Random();
 
     private int x = rnd.nextInt(2);//male rat number
     private int y = rnd.nextInt(2);//female rat number
 
-    private int playerX = 1;
-    private int playerY = 1;
+    private int ratX = 1;
+    private int ratY = 1;
 
     private int[] maleRats;
     private int[] femaleRats;
 
 
-    public void drawRat(){
+    public void draw(){
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         for(int i=0 ;i<WINDOW_WIDTH; i+=5){
             if(i <= x){
-                gc.drawImage(maleRat, playerX,playerY * WINDOW_HEIGHT);
-                gc.drawImage(femaleRat, playerX,playerY * WINDOW_WIDTH);
+                gc.drawImage(maleRat, ratX,ratY * WINDOW_HEIGHT);
+                gc.drawImage(femaleRat, ratX,ratY * WINDOW_WIDTH);
             }
         }
     }
     public void tick() {
 
-        playerX = playerX + 1;
-        drawRat();
+        ratX = ratX + 1;
+        draw();
 
-        if (playerX > 11) {
+        if (ratX > 11) {
             for(int i=11; i>0 ; i--)
-            playerX = playerX - 1;
-            drawRat();
+            ratX = ratX - 1;
+            draw();
         }
         // We then redraw the whole canvas.
 
