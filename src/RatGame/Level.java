9165e3e5ac4051ee36c627687edcf41662a97be9
Benas Montrimas <benasm123@googlemail.com>
2021-11-17 10:48:10 +0000

    Tidy up level class

--- src/RatGame/Level.java
+++ src/RatGame/Level.java
@@ -20,34 +20,40 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Level {
-    String levelName;
-
-    private static final int GRID_HEIGHT = 50;
-    private static final int GRID_WIDTH = 50;
-
+    private String levelName;
     private int levelHeight;
     private int levelWidth;
 
-    char[][] levelGrid;
+    private static final int TILE_HEIGHT = 50;
+    private static final int TILE_WIDTH = 50;
+
+    private char[][] levelGrid;
 
     private double lastMouseX;
     private double lastMouseY;
 
-    @FXML
-    private AnchorPane GameScreen;
-
+    // TODO Remove, these should be stored in the tiles
     Image grassTile = new Image("Assets/Grass.png");
     Image pathTile = new Image("Assets/Path.png");
     Image tunnelTile = new Image("Assets/Tunnel.png");
     Image tunnelVertTile = new Image("Assets/TunnelVertical.png");
 
+    // All FXML variables.
+    @FXML
+    private AnchorPane GameScreen;
+
     @FXML
     private Canvas GameBoard;
+
 
     public void initialize(){
     }
 
-
+    /**
+     * Switch scene to the main menu.
+     * @param event The action that triggered this.
+     * @throws IOException If file does not exist will throw exception.
+     */
     public void switchToMainMenu(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FXML/mainMenu.fxml")));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -55,11 +61,19 @@ public class Level {
         scene.setRoot(root);
     }
 
+    /**
+     * Stores the mouse position when pressed down.
+     * @param event The mouse click event is stored here.
+     */
     public void getMousePosOnClicked(MouseEvent event){
         lastMouseX = event.getX();
         lastMouseY = event.getY();
     }
 
+    /**
+     * moves the level around when dragging. Keeping in bounds of the screen.
+     * @param event The mouse event is stored here.
+     */
     public void onDragLevel(MouseEvent event){
         GameBoard.setTranslateX(GameBoard.getTranslateX() + (event.getX() - lastMouseX));
         GameBoard.setTranslateY(GameBoard.getTranslateY() + (event.getY() - lastMouseY));
@@ -136,8 +150,8 @@ public class Level {
             levelWidth = Integer.parseInt(levelDimensionsSplit[0]);
             levelHeight = Integer.parseInt(levelDimensionsSplit[1]);
 
-            GameBoard.setHeight(GRID_HEIGHT * levelHeight);
-            GameBoard.setWidth(GRID_WIDTH * levelWidth);
+            GameBoard.setHeight(TILE_HEIGHT * levelHeight);
+            GameBoard.setWidth(TILE_WIDTH * levelWidth);
 
             levelGrid = new char[levelHeight][levelWidth];
             for (int row = 0; row < levelHeight; row++) {
@@ -155,7 +169,6 @@ public class Level {
         spawnTiles(GameBoard.getGraphicsContext2D());
     }
 
-    // TODO Create a save/load class which will hold all these methods for saving player and level.
     // TODO Once we have a player class store all saves for each player in the player folder instead.
     // TODO If a level save is already active for a player we need to only let the player resume.
     public void saveLevel(ActionEvent event){
@@ -171,17 +184,23 @@ public class Level {
         }
     }
 
-    public void spawnTiles(GraphicsContext gc){
+    // TODO Once we have a tiles class we will need to use those instead of hard coding to draw.#
+
+    /**
+     * Draws the tiles onto the level.
+     * @param gc Takes in the graphics content of what we want to draw on.
+     */
+    private void spawnTiles(GraphicsContext gc){
         for (int row = 0; row < levelHeight; row++) {
             for (int col = 0; col < levelWidth; col++) {
                 if (levelGrid[row][col] == 'G'){
-                    gc.drawImage(grassTile, col * GRID_HEIGHT, row * GRID_WIDTH);
+                    gc.drawImage(grassTile, col * TILE_HEIGHT, row * TILE_WIDTH);
                 } else if (levelGrid[row][col] == 'P'){
-                    gc.drawImage(pathTile, col * GRID_HEIGHT, row * GRID_WIDTH);
+                    gc.drawImage(pathTile, col * TILE_HEIGHT, row * TILE_WIDTH);
                 } else if (levelGrid[row][col] == 'T'){
-                    gc.drawImage(tunnelTile, col * GRID_HEIGHT, row * GRID_WIDTH);
+                    gc.drawImage(tunnelTile, col * TILE_HEIGHT, row * TILE_WIDTH);
                 } else if (levelGrid[row][col] == 'V'){
-                    gc.drawImage(tunnelVertTile, col * GRID_HEIGHT, row * GRID_WIDTH);
+                    gc.drawImage(tunnelVertTile, col * TILE_HEIGHT, row * TILE_WIDTH);
                 } else {
                     System.out.print("Hmm there shouldn't be any other tile????");
                     assert true;
@@ -189,5 +208,4 @@ public class Level {
             }
         }
     }
-
 }
