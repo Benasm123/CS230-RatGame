9a4328325eef52a6681255de4091cfdfb2bd7b4c
ECO010 <59510397+ECO010@users.noreply.github.com>
2021-11-17 22:22:14 +0000

    Tile class update
    little update to the Tile class

--- src/RatGame/Tile.java
+++ src/RatGame/Tile.java
@@ -3,9 +3,10 @@ package RatGame;
 import javafx.scene.image.Image;
 
 public class Tile <E> {
+
     private float posX, posY;
     private int tileHeight, tileWidth;
-    private Tile type;
+    private TileType type;
     private E e; // the element contained in the linked list
     private Tile<E> next; // the next element of the linked list
 
@@ -14,18 +15,7 @@ public class Tile <E> {
     Image tunnelTile = new Image("Assets/Tunnel.png");
     Image tunnelVertTile = new Image("Assets/TunnelVertical.png");
 
-    Image babyRat = new Image("Assets/Baby.png");
-    Image femaleRat = new Image("Assets/Female.png");
-    Image maleRat;
-    Image deathRat;
-
-    Image bomb;
-    Image gas;
-    Image poison;
-    Image sterilization;
-    Image stop;
-
-    public Tile(float posX, float posY, int tileHeight, int tileWidth, Tile type) {
+    public Tile(float posX, float posY, int tileHeight, int tileWidth, TileType type) {
         this.posX = posX;
         this.posY = posY;
         this.tileHeight = tileHeight;
@@ -44,4 +34,13 @@ public class Tile <E> {
     public void find() {
 
     }
+
+    public void draw() {
+
+    }
+
+    public TileType getType() {
+        return type;
+    }
+
 }
