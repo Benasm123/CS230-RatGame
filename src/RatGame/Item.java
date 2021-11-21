91da5645d73d02ad05e06210d7fb04df56d6524f
Benas Montrimas <benasm123@googlemail.com>
2021-11-21 23:01:42 +0000

    Updated item to include stepped on and x y coords

--- src/RatGame/Item.java
+++ src/RatGame/Item.java
@@ -5,6 +5,10 @@ import javafx.scene.image.Image;
 public abstract class Item {
 
     Image texture;
-    public abstract void use ();
+    int xPos;
+    int yPos;
+
+    public abstract void use();
+    public abstract void steppedOn();
     
 }
