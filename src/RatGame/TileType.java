af3829709acea0e9f2a99f559e4111da820921e2
Benas Montrimas <benasm123@googlemail.com>
2021-11-21 23:52:47 +0000

    set tunnels as traversable

--- src/RatGame/TileType.java
+++ src/RatGame/TileType.java
@@ -5,8 +5,8 @@ import javafx.scene.image.Image;
 public enum TileType {
     Grass (new Image("Assets/Grass.png"), false),
     Path(new Image("Assets/Path.png"),true),
-    Tunnel(new Image("Assets/Tunnel.png"),false),
-    VerticalTunnel (new Image("Assets/TunnelVertical.png"),false);
+    Tunnel(new Image("Assets/Tunnel.png"),true),
+    VerticalTunnel (new Image("Assets/TunnelVertical.png"),true);
 
     Image textureName;
     boolean isTraversable;
