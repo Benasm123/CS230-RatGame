4c3777f6b1486f8e03f8d8ba94836eda20c80173
Benas Montrimas <benasm123@googlemail.com>
2021-11-22 14:25:05 +0000

    Updated screen size

--- src/RatGame/Main.java
+++ src/RatGame/Main.java
@@ -2,21 +2,27 @@ package RatGame;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
     // The dimensions of the window
     private static final int WINDOW_WIDTH = 1200;
     private static final int WINDOW_HEIGHT = 800;
+
+    Rectangle2D screenSize = Screen.getPrimary().getBounds();
+
     //test
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("FXML/mainMenu.fxml"));
         primaryStage.setTitle("Rat Game");
 
-        primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
+        primaryStage.setScene(new Scene(root, Math.min(WINDOW_WIDTH, screenSize.getWidth()), Math.min(WINDOW_HEIGHT, screenSize.getHeight())));
         primaryStage.show();
     }
 
