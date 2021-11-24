1056d2bfc0b198ac4f7bdfaba30597fab6b18915
Jack123lewis <jacklewistmc@hotmail.co.uk>
2021-11-24 18:26:30 +0000

    Added variables and a method to the Gas and Sterilisation classes.

--- src/RatGame/Gas.java
+++ src/RatGame/Gas.java
@@ -4,13 +4,22 @@ import javafx.scene.image.Image;
 
 public class Gas extends Item{
 
+    private static final int lifeDuration = 5;
+    private static int lifeRemaining;
+    private static final int speed = 5;
     private float posX;
     private float posY;
     Image texture;
 
     public Gas() {
+
         texture = new Image("Assets/Gas.png");
     }
+
+    public void spreadGas(){
+
+    }
+
 
     @Override
     public void use() {
