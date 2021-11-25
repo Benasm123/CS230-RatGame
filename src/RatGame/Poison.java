97d566c05a047b0a23747dff1d799b2c9a7b8c77
ECO010 <59510397+ECO010@users.noreply.github.com>
2021-11-25 22:30:54 +0000

    updates to classes

--- src/RatGame/Poison.java
+++ src/RatGame/Poison.java
@@ -4,25 +4,20 @@ import javafx.scene.image.Image;
 
 public class Poison extends Item {
 
-    //private boolean isOnPath;
-
     public Poison() {
         texture =  new Image("Assets/Poison.png");
+        imageView.setImage(texture);
     }
-
-    public void kill() {
-
-    }
-
 
     @Override
     public void use() {
-
+        expired = true;
     }
 
     @Override
     public void steppedOn(Rat rat) {
-
+        //rat.setDead();
+        use();
     }
 
     @Override
