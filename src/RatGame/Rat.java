e2397a6e086b077da8cdd63ab5b6ef861ad475e4
TimiAbajingin123 <54741402+TimiAbajingin123@users.noreply.github.com>
2021-11-28 02:40:40 +0000

    Update Rat.java

--- src/RatGame/Rat.java
+++ src/RatGame/Rat.java
@@ -11,7 +11,6 @@ import java.util.Random;
 // and setters.
 
 public class Rat {
-    // I think better to call this texture, as Rat.rat isnt going to be clear as what this variable acutally holds.
     Image texture;
     ImageView img;
 
@@ -24,10 +23,6 @@ public class Rat {
     private float xVel=5.0f;
     private float yVel=0.0f;
 
-    // rat doesnt care about what other rats exist, only itself. Dont think as this class as all the rats, but each individual rat.
-    // So each instance will only ever care about itself and no other rat (Other than when they need to interact, but well manage that in the level class).
-    private int[] maleRats;
-    private int[] femaleRats;
 
     // You want to set this in the constructor as you want hte lastX/Y to be set on where the rat spawns, but if you do it here it will always initialize to 0.
     int lastX;
@@ -55,10 +50,7 @@ public class Rat {
         this.type = type;
         lastX = (int)xPos;
         lastY = (int)yPos;
-        // You can just put isDeathRat, as its a bool it will return either true or false already == true is redundant and messy imo.
-        // This looks good, but again id just refactor the rat variable to texture
-        // Also it would be a lot better if instead of a string for type you had an enum (like in the Tile class) that way typos wont be a common bug.
-        // Then id have ratType.DeathRat or RatType.BabyRat or RatType.AdultRat... and so on
+
         img = new ImageView();
         if(type == ratType.DEATHRAT && isBaby==false){
             texture = new Image("Assets/Death.png");
@@ -104,8 +96,6 @@ public class Rat {
     }
 
     // Added Level grid as a parameter here and in checkPaths so that i can pass it in to the update method so you have access to the level.
-    // This need to be tidied up, lots of repeated code and a pain to work with, i know this is how ive done it but it was
-    // for testing purposes.
     public void move(float deltaTime, Tile[][] levelGrid){
         xPos += xVel * deltaTime;
         yPos += yVel * deltaTime;
@@ -130,8 +120,7 @@ public class Rat {
 
     }
 
-    // this can setRotation as well, as this always refers to the current instance of rat you can just set rotation to the return value and not return anything.
-    public float getRotation() {
+    public float setGetRotation(ImageView img) {
         if (xVel < 0) {
             rotation = 90.0f;
         } else if (xVel > 0) {
@@ -141,20 +130,14 @@ public class Rat {
         } else {
             rotation = 180.0f;
         }
+        img.setRotate(rotation);
         return rotation;
-    }
-
-    // yeah here this is redundant you can just copy the code above and set rotation instead of return and set getRotation to just return this.rotation or however java does that.
-    // Oh also just noticed your changing the rotation of the img directly, i would use this to set the variable you have called rotation. Because we might need rotation later for more
-    // uses and then when we need to actually draw the image well set the rotation of the rat there.
-    public void setRotation(ImageView img){
-        img.setRotate(getRotation());
     }
 
     // Commented out move as right now this is trying ot check a null array.
     public void update(float deltaTime, Tile[][] levelGrid){
         this.move(deltaTime, levelGrid);
-        this.setRotation(img);
+        this.setGetRotation(img);
         if(isBaby==true){
             growUp(deltaTime);
         }
