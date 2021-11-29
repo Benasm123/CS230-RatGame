458694c1852880bd8d30e1e3267fc2fc3768bdad
Jack123lewis <jacklewistmc@hotmail.co.uk>
2021-11-29 14:07:10 +0000

    Updated Gas and Sterilisation classes with Javadoc and update methods

--- src/RatGame/Gas.java
+++ src/RatGame/Gas.java
@@ -1,10 +1,19 @@
+/**
+ * This class allows for the creation of Gas items.
+ * The gas item kills rats that are in the gas particles for too long.
+ *
+ * @author CS-230 Group13 (21/22)
+ * @version 1.0
+ *
+ */
+
 package RatGame;
 
 import javafx.scene.image.Image;
 
 public class Gas extends Item{
 
-    private static final int lifeDuration = 5;
+    private static final int lifeDuration = 10;
     private static int lifeRemaining;
     private static final int speed = 5;
 
@@ -17,19 +26,35 @@ public class Gas extends Item{
 
     }
 
-
+    /**
+     * method inherited from the parent class.
+     */
     @Override
     public void use() {
 
     }
+
+    /**
+     *
+     * @param rat
+     */
 
     @Override
     public void steppedOn(Rat rat) {
 
     }
 
+    /**
+     *
+     * @param deltaTime
+     */
     @Override
     public void update(float deltaTime) {
+        lifeRemaining -= deltaTime;
+        spreadGas();
+        if (lifeRemaining == 0) {
 
+            expired = true;
+        }
     }
 }
