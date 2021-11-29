d2288020e319a9b7cdd79ad200641496dddb12ac
ECO010 <59510397+ECO010@users.noreply.github.com>
2021-11-29 22:36:36 +0000

    Update Bomb.java

--- src/RatGame/Bomb.java
+++ src/RatGame/Bomb.java
@@ -11,42 +11,26 @@ package RatGame;
 
 import javafx.scene.image.Image;
 import javafx.util.Pair;
-
 import java.util.ArrayList;
 
 public class Bomb extends Item {
 
     private static final int COUNTDOWN = 4;
     private float timeSincePlaced = 0;
+    boolean isExploding;
 
     public Bomb() {
         texture = new Image("Assets/Bomb.png");
+        isExploding = false;
     }
 
     /**
      * explodes vertically and horizontally till it reaches Grass tiles in both directions,
      * deletes everything in path (rat, item)
      */
-    public void explode(Tile[][] levelGrid, Item item, Rat rat) {
-
-        final boolean canExplodeRight = levelGrid[xPos + 1][yPos].getType().isTraversable;
-        final boolean canExplodeLeft = levelGrid[xPos - 1][yPos].getType().isTraversable;
-        final boolean canExplodeUp = levelGrid[xPos][yPos + 1].getType().isTraversable;
-        final boolean canExplodeDown = levelGrid[xPos][yPos - 1].getType().isTraversable;
-        final boolean isExploding = canExplodeDown || canExplodeLeft || canExplodeRight || canExplodeUp;
-
-//        while (isExploding) {
-//            if (item.xPos && item.yPos == getBombTiles()){
-//                item.expired = true;
-//            }
-//            if (rat.getxPos() && rat.getyPos() == getBombTiles()) {
-//                rat.die();
-//            }
-//        }
-        expired = true;
-    }
 
     public ArrayList<Pair<Integer, Integer>> getBombTiles(Tile[][] levelGrid) {
+        isExploding = false;
         ArrayList<Pair<Integer, Integer>> bombTiles = new ArrayList<>();
         int lastCheckedX = xPos;
         int lastCheckedY = yPos;
@@ -54,33 +38,35 @@ public class Bomb extends Item {
         final boolean canExplodeLeft = levelGrid[xPos - 1][yPos].getType().isTraversable;
         final boolean canExplodeUp = levelGrid[xPos][yPos + 1].getType().isTraversable;
         final boolean canExplodeDown = levelGrid[xPos][yPos - 1].getType().isTraversable;
-        final boolean isExploding = canExplodeDown || canExplodeLeft || canExplodeRight || canExplodeUp;
 
-        if (isExploding) {
-            while (canExplodeLeft && xPos - 1 != lastCheckedX) {
-                bombTiles.add(new Pair<>(xPos,yPos));
-                lastCheckedX--;
-            }
-            lastCheckedX = xPos;
-            lastCheckedY = yPos;
-            while (canExplodeUp && yPos + 1 != lastCheckedY) {
-                bombTiles.add(new Pair<>(xPos,yPos));
-                lastCheckedY++;
-            }
-            lastCheckedX = xPos;
-            lastCheckedY = yPos;
-            while (canExplodeRight && xPos + 1 != lastCheckedX) {
-                bombTiles.add(new Pair<>(xPos,yPos));
-                lastCheckedX++;
-            }
-            lastCheckedX = xPos;
-            lastCheckedY = yPos;
-            while (canExplodeDown && yPos - 1 != lastCheckedY) {
-                bombTiles.add(new Pair<>(xPos,yPos));
-                lastCheckedY--;
-            }
-            lastCheckedX = xPos;
-            lastCheckedY = yPos;
+        while (canExplodeLeft && xPos - 1 != lastCheckedX) {
+            bombTiles.add(new Pair<>(xPos,yPos));
+            lastCheckedX--;
+        }
+        lastCheckedX = xPos;
+        lastCheckedY = yPos;
+        while (canExplodeUp && yPos + 1 != lastCheckedY) {
+            bombTiles.add(new Pair<>(xPos,yPos));
+            lastCheckedY++;
+        }
+        lastCheckedX = xPos;
+        lastCheckedY = yPos;
+        while (canExplodeRight && xPos + 1 != lastCheckedX) {
+            bombTiles.add(new Pair<>(xPos,yPos));
+            lastCheckedX++;
+        }
+        lastCheckedX = xPos;
+        lastCheckedY = yPos;
+        while (canExplodeDown && yPos - 1 != lastCheckedY) {
+            bombTiles.add(new Pair<>(xPos,yPos));
+            lastCheckedY--;
+        }
+        lastCheckedX = xPos;
+        lastCheckedY = yPos;
+        //for debugging
+        for (Pair<Integer, Integer> i: bombTiles
+        ) {
+            System.out.println("x:" + i.getKey() + " y:" + i.getValue());
         }
         return bombTiles;
     }
@@ -110,7 +96,8 @@ public class Bomb extends Item {
     public void update(float deltaTime) {
         timeSincePlaced += deltaTime;
         if (timeSincePlaced >= COUNTDOWN) {
-//            explode();
+            isExploding = true;
+            expired = true;
         }
     }
 }
