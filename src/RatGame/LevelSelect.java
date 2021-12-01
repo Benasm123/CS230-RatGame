8cacb6313eaccdf38a9e9bf3972650f096b270e1
Zzz000z <josemsm7@gmail.com>
2021-12-01 14:03:45 +0000

    Leaderboard is displayer on level select

--- src/RatGame/LevelSelect.java
+++ src/RatGame/LevelSelect.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -18,7 +19,11 @@ public class LevelSelect {
 
     @FXML
     VBox LevelButtons;
-
+    @FXML
+    VBox LeaderboardButtons;
+    @FXML
+	private Text top10;
+    
     public void initialize(){
         String[] allLevels = new File("src/Levels/").list();
 
@@ -47,6 +52,39 @@ public class LevelSelect {
             }
             LevelButtons.getChildren().add(levelSelectButton);
         }
+        
+        String[] allLeaderboards = new File("src/Leaderboards/").list();
+
+        if (allLeaderboards == null)
+        {
+        	new Leaderboard(0);
+        	new Leaderboard(1);
+        	new Leaderboard(2);
+        	new Leaderboard(3);
+        	new Leaderboard(4);
+        	new Leaderboard(5);
+        	new Leaderboard(6);
+        	new Leaderboard(7);
+        	new Leaderboard(8);
+        	new Leaderboard(9);
+        	allLeaderboards = new File("src/Leaderboards/").list();
+        }
+
+        for (String j : allLeaderboards)
+        {
+
+            Button leaderboardSelectButton = new Button(j);
+            leaderboardSelectButton.setOnAction(event -> 
+            {
+                try {
+                    displayPressed(event);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+//                }
+            LeaderboardButtons.getChildren().add(leaderboardSelectButton);
+        }
     }
 
     private void playPressed(ActionEvent event) throws IOException {
@@ -64,6 +102,12 @@ public class LevelSelect {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = stage.getScene();
         scene.setRoot(root);
+    }
+    public void displayPressed(ActionEvent event)throws IOException {
+        Leaderboard board = new Leaderboard();
+        int lvl = Integer.parseInt(String.valueOf(((Button)event.getSource()).getText().charAt(0)));
+        board.load(lvl);
+    	top10.setText(board.toString());
     }
 
 }
