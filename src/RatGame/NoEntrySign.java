4ce0c73b46cd26b7d1191d2c626f8c919f02211c
Zzz000z <josemsm7@gmail.com>
2021-12-01 00:44:15 +0000

    no entry sign now displays damage

--- src/RatGame/NoEntrySign.java
+++ src/RatGame/NoEntrySign.java
@@ -6,23 +6,28 @@ import javafx.scene.image.Image;
 public class NoEntrySign extends Item 
 {
     // Maybe make this a bit more meaningful, like STARTING_HEALTH or something
-	private static final int HP = 5;
+	private static final int MAX_HP = 6;
 	private int hpLeft;
 	
 	public NoEntrySign()
 	{
-		hpLeft = HP;
-        texture = new Image("Assets/Stop.png");
+		hpLeft = MAX_HP;
+        setTexture(new Image("Assets/Stop.png"));
 	}
     
     @Override
     public void use() {
         System.out.println("Stop sign used?");
     }
-
+    
+    private void updateImage(int hp)
+    {
+    	getImageView().setImage(new Image("Assets/brokenstop/brokenstop" + hp + ".png"));
+    }
     @Override
     public void steppedOn(Rat rat) {
         hpLeft--;
+        updateImage(hpLeft);
         rat.setxVel(rat.getxVel()*-1);
         rat.setyVel(rat.getyVel()*-1);
     	if (hpLeft == 0)
