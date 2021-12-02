ee8372f4c23e308ee96627e4a2926c450372093e
Almahari-A <a7md13ebrahim@gmail.com>
2021-12-02 13:25:05 +0000

    Javadoc and removed setType as i think its not needed

--- src/RatGame/DeathRat.java
+++ src/RatGame/DeathRat.java
@@ -2,13 +2,22 @@ package RatGame;
 
 import javafx.scene.image.Image;
 
-
+/**
+ * This class allows for the creation of death rat item.
+ * The death rat kills 5 other rats.
+ *
+ * @author Ahmed Almahari
+ *
+ */
 public class DeathRat extends Item {
     private float timeLeft;
     private boolean isSpawning;
 
     private static final int TIMER = 2;
 
+    /**
+     * Creates an instance of an item of type death rat.
+     */
     public DeathRat() {
         timeLeft = TIMER;
         texture = new Image("Assets/Death.png");
@@ -19,23 +28,36 @@ public class DeathRat extends Item {
         isSpawning = spawning;
     }
 
+    /**
+     *
+     * @return isSpawning
+     */
     public boolean getSpawning() {
         return isSpawning;
     }
 
-    public void setType(ItemType type) {
-    }
-
+    /**
+     * method inherited from the parent class, called when the item is used
+     */
     @Override
     public void use() {
 
     }
 
+    /**
+     *
+     * @param rat
+     */
     @Override
     public void steppedOn(Rat rat) {
 
     }
 
+    /**
+     *
+     * method which keeps rat stationary when spawning.
+     * @param deltaTime The time since the last frame in seconds.
+     */
     @Override
     public void update(float deltaTime) {
         timeLeft -= deltaTime;
